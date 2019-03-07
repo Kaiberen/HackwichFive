022cc1ebd75b7ab3487507c5032026435e5a33cd
Kaiberenn <Kaiberen@hawaii.edu>
2019-03-07 10:36:07 -1000

    HackwichFive part 1-8 complete

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+  
+    
+    @IBAction func sliderHasMoved(_ sender: Any) {
+       
+        print("The value of the slider is:\(slider.value)")
+    }
+    
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+       
+        let alert = UIAlertController(title:"Hello World!", message:  "This is my first alert view", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
 }
 
